05e58a222dcf98aa89b6b0c41e4538b67ae90005
alyshanmaredia <alyshanmaredia@gmail.com>
2020-12-27 15:44:31 +0500

    connect to localmongodb

---
+++ server/server.js
@@ -0,0 +1,42 @@
+require('dotenv').config()
+const express = require('express')
+const mongoose = require('mongoose')
+const cors = require('cors')
+const cookieParser = require('cookie-parser')
+const fileUpload = require('express-fileupload')
+
+
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+app.use(cookieParser())
+app.use(fileUpload({
+    useTempFiles: true
+}))
+
+const URI = process.env.MONGODB_URL
+mongoose.connect(URI, {
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}, err=>{
+    if(err){
+        console.log("Not Connected to MongoDB!!"); 
+        throw err; 
+    } else{
+        console.log("Connected to MongoDB!!");
+    }
+    
+
+})
+
+app.use('/', (req,res,next)=>{
+    res.json({msg: "Hello Home!"})
+})
+
+const PORT = process.env.PORT || 5000
+app.listen(PORT, ()=>{
+    console.log("Server is running on", PORT)
+})
