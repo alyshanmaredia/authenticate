5b9a385558bbfee8ce6ac9103a6571edfee38a56
alyshanmaredia <alyshanmaredia@gmail.com>
2020-12-27 16:23:11 +0500

    added User Schema

---
+++ server/models/userModel.js
@@ -0,0 +1,31 @@
+const mongoose = require('mongoose')
+
+const userSchema = new mongoose.Schema({
+    name:{
+        type: String,
+        required: [true, "Please Enter your name!"],
+        trim: true
+    },
+    email: {
+        type: String,
+        required: [true, "Please Enter your Email!"],
+        trim: true,
+        unique: true
+    },
+    password:{
+        type: String,
+        required: [true, "Please Enter your Password!"],
+        
+    },
+    role:{
+        type: Number,
+        default: 0
+    },
+    avatar:{
+        type: String,
+        default:"a"
+    }   
+},  {
+        timestamps: true
+    }
+})
